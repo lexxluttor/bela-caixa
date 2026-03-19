5831b20c8b16acc6a867d146e72499a4556df1a4
lexxluttor <lexxluttor7@gmail.com>
2026-03-19 10:55:18 -0300

    Update sheets-config.js

--- sheets-config.js
+++ sheets-config.js
@@ -1,1 +1,2 @@
-https://script.google.com/macros/s/AKfycbylVqvMnF99hHM-1QOeFOYMU-cZgHE38WgVcm73Y7u48_X6JsQ4Ne7CzozTv6kEAoMu/exec
+window.BELA_SHEETS_API_URL = "SEU_LINK_EXEC_AQUI";
+window.BELA_SHEETS_SYNC_INTERVAL_MS = 120000;
