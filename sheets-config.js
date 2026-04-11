846f563c90e30429e1108bd630652d58d31df51b
lexxluttor <lexxluttor7@gmail.com>
2026-04-11 09:47:19 -0300

    Refactor API URL and sync interval constants

--- sheets-config.js
+++ sheets-config.js
@@ -1,2 +1,3 @@
-janela.URL_da_API_BELA_SHEETS = "https://script.google.com/macros/s/AKfycbzFg9UzVHII6bMui0JV1dP4lXQ7L5D3AXWqTNtv95n3-pSnME-gMNU3gMqxPVrEECq-/exec";
-janela.BELA_SHEETS_SYNC_INTERVAL_MS = 120000;
+const API_BELA_SHEETS = "https://script.google.com/macros/s/AKfycbwyKpgMBgweaekrYEsT22NjCj-d2WI9lpiZUzg.../exec";
+
+const SHEETS_SYNC_INTERVAL_MS = 120000;
