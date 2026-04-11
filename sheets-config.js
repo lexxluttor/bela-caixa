53589c71a54ae5826103d68bad73fa93b81444c0
lexxluttor <lexxluttor7@gmail.com>
2026-04-11 11:23:56 -0300

    Update API_BELA_SHEETS URL in sheets-config.js

--- sheets-config.js
+++ sheets-config.js
@@ -1,3 +1,3 @@
-const API_BELA_SHEETS = "https://script.google.com/macros/s/AKfycbwyKpgMBgweaekrYEsT22NjCj-d2WI9lpiZUzg.../exec";
+const API_BELA_SHEETS = "https://script.google.com/macros/s/AKfycbxvE2DpOpZDW1bZOvatqdN0HjSOXI3gvFdGPSj7qeUb6NF2V-K18-5tpil1KGW4O1lB/exec";
 
 const SHEETS_SYNC_INTERVAL_MS = 120000;
